89b73514480f1dbbec165c8b5850c701e7869bbe
Khanbikov-Askhat <Askhat.Khanbikov@Avon.com>
2024-04-23 07:50:25 +0300

    Sprint 15

--- src/main/java/ru/practicum/shareit/booking/dto/BookingRequest.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingRequest.java
@@ -1,16 +1,19 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class BookingRequest {
-
     private Long id;
     @NotNull
     private Long itemId;
